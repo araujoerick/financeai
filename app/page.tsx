f494487a54327de81428085a34f66862101d0ace
Erick Araújo <araujoerick.dev@gmail.com>
2024-11-06 10:44:53 -0300

    :wrench: chore: setup shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "./_components/ui/button";
+
 export default function Home() {
   return (
-    <h1 className="flex w-screen items-center justify-center text-red-400">
-      Hello
-    </h1>
+    <>
+      <h1 className="flex w-screen items-center justify-center text-red-400">
+        Hello
+      </h1>
+      <Button>Hello World</Button>
+    </>
   );
 }
