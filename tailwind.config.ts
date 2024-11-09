d7333b264d2c9f6b9d668e7117aa35ddadf64251
Erick Araújo <araujoerick.dev@gmail.com>
2024-11-09 18:10:58 -0300

    ✨ feat: Ensure transaction table labels are rendered properly

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -60,7 +60,6 @@ const config: Config = {
       },
     },
   },
-  // eslint-disable-next-line @typescript-eslint/no-require-imports
   plugins: [require("tailwindcss-animate")],
 };
 export default config;
