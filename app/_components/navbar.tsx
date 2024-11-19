e456028c96e441a4b96660d898bea6681c7c1d41
Erick Araújo <araujoerick.dev@gmail.com>
2024-11-18 23:22:45 -0300

    ✨ feat: add popover menu to navbar in small devices

--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -4,7 +4,9 @@ import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import React from "react";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+import { MenuIcon } from "lucide-react";
+import { Separator } from "./ui/separator";
 
 const Navbar = () => {
   const pathname = usePathname();
@@ -12,40 +14,85 @@ const Navbar = () => {
   return (
     <nav className="border-b border-solid py-4">
       <div className="m-auto flex max-w-[2012px] justify-between px-6">
-        <div className="flex items-center gap-10">
+        <div className="flex items-center gap-4">
           <Image src="/logo.svg" alt="Finance AI" width={173} height={39} />
-          <Link
-            href="/"
-            className={
-              pathname === "/"
-                ? "font-bold text-primary"
-                : "text-muted-foreground"
-            }
-          >
-            Dashboard
-          </Link>
-          <Link
-            href="/transactions"
-            className={
-              pathname === "/transactions"
-                ? "font-bold text-primary"
-                : "text-muted-foreground"
-            }
-          >
-            Transações
-          </Link>
-          <Link
-            href="/subscription"
-            className={
-              pathname === "/subscription"
-                ? "font-bold text-primary"
-                : "text-muted-foreground"
-            }
-          >
-            Assinatura
-          </Link>
+          {/* Menu Desktop */}
+          <div className="hidden items-center gap-6 md:flex">
+            <Link
+              href="/"
+              className={
+                pathname === "/"
+                  ? "font-bold text-primary"
+                  : "text-muted-foreground hover:text-white"
+              }
+            >
+              Dashboard
+            </Link>
+            <Link
+              href="/transactions"
+              className={
+                pathname === "/transactions"
+                  ? "font-bold text-primary"
+                  : "text-muted-foreground hover:text-white"
+              }
+            >
+              Transações
+            </Link>
+            <Link
+              href="/subscription"
+              className={
+                pathname === "/subscription"
+                  ? "font-bold text-primary"
+                  : "text-muted-foreground hover:text-white"
+              }
+            >
+              Assinatura
+            </Link>
+          </div>
+          {/* Menu Mobile */}
         </div>
-        <UserButton showName />
+        <Popover>
+          <PopoverTrigger className="block md:hidden">
+            <MenuIcon className="h-6 w-6" />
+          </PopoverTrigger>
+          <PopoverContent className="flex w-fit flex-col gap-2 p-2">
+            <Link
+              href="/"
+              className={
+                pathname === "/"
+                  ? "font-bold text-primary"
+                  : "text-muted-foreground hover:text-white"
+              }
+            >
+              Dashboard
+            </Link>
+            <Link
+              href="/transactions"
+              className={
+                pathname === "/transactions"
+                  ? "font-bold text-primary"
+                  : "text-muted-foreground hover:text-white"
+              }
+            >
+              Transações
+            </Link>
+            <Link
+              href="/subscription"
+              className={
+                pathname === "/subscription"
+                  ? "font-bold text-primary"
+                  : "text-muted-foreground hover:text-white"
+              }
+            >
+              Assinatura
+            </Link>
+            <Separator />
+            <UserButton showName />
+          </PopoverContent>
+        </Popover>
+        <div className="hidden md:flex">
+          <UserButton showName />
+        </div>
       </div>
     </nav>
   );
